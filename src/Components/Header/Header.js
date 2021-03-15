3fcd8e3cd0ed4486afa7d09daa3ba5d1a980446e
surajsrv11 <surajvishwakarma625@gmail.com>
2021-03-15 17:50:10 +0530

    initial commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ()=>{
+    return(
+        <div>
+            <header>
+                <ul>
+                    <li><img src="#"></img></li>
+                    <li>Projects</li>
+                    <li>Resume</li>
+                    <li>Contact Me</li>
+                </ul>
+            </header>
+        </div>
+    )
+}
+
+
+export default Header
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Components/Header/Header.js'
+import './style.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
